29fd984b72b18434b0c811f255d8ddebeb5ad57b
Aaravshah2907 <74358472+Aaravshah2907@users.noreply.github.com>
2023-06-06 16:23:53 +0530

    Update trigoinverse.py
    An updated Version

--- trigoinverse.py
+++ trigoinverse.py
@@ -1,59 +1,62 @@
 import math as m
 
 print("Choose from the following ")
-print("1. arcsin")
-print("2. arccos")
-print("3. arctan")
-print("4. arccot")
-print("5. arcsec")
-print("6. arccosec")
+print("1. arc sin")
+print("2. arc cos")
+print("3. arc tan")
+print("4. arc cot")
+print("5. arc sec")
+print("6. arc cosec")
 
 inverse_choice = int(input("Enter Choice : "))
 number_choice = float(input("Enter a value  of whose inverse you want to find: "))
 
 
-def arcsin(var):
+def arc_sin(var):
     if var**2 - 1 <= 0:
         result1 = str(m.asin(var))
-        result2 = str(m.asin(var))
     else:
-        pi2 = (m.pi)/2
+        pi2 = m.pi/2
         calc1 = m.log(var + (var**2 - 1)**0.5)
-        result1 = (str(pi2) + " - (i " + str(calc1)+")")
-        result2 = (str(pi2) + " + (i " + str(calc1)+")")
-    return result1, result2
+        result1 = (str(pi2) + " ± (" + str(calc1)+")i")
+
+    return result1
 
 
-def arccos(var):
+def arc_cos(var):
     if var**2 - 1 <= 0:
         result1 = m.asin(var)
-        result2 = m.asin(var)
     else:
         calc1 = m.log(var + (var**2 - 1)**0.5)
-        result1 = ("- (i " + str(calc1)+")")
-        result2 = ("+ (i " + str(calc1)+")")
-    return result1, result2
+        result1 = ("± (" + str(calc1)+")i")
+    return result1
+
+
+def arc_tan(var):
+    result = m.atan(var)
+    return result
 
 
 if inverse_choice == 1:
-    decision = 'arcsin'
-    calculated_output1, calculated_output2 = arcsin(number_choice)
+    decision = 'arc sin'
+    calculated_output = arc_sin(number_choice)
 elif inverse_choice == 2:
-    decision = 'arccos'
-    calculated_output1, calculated_output2 = arccos(number_choice)
+    decision = 'arc cos'
+    calculated_output = arc_cos(number_choice)
 elif inverse_choice == 3:
-    decision = 'arctan'
-    calculated_output1, calculated_output2 = m.atan(number_choice)
+    decision = 'arc tan'
+    calculated_output = arc_tan(number_choice)
 elif inverse_choice == 4:
-    decision = 'arccot'
-    calculated_output1, calculated_output2 = m.atan(1/number_choice)
+    decision = 'arc cot'
+    calculated_output = arc_tan(1/number_choice)
 elif inverse_choice == 5:
-    decision = 'arcsec'
-    calculated_output1, calculated_output2 = arccos(1/number_choice)
+    decision = 'arc sec'
+    calculated_output = arc_cos(1/number_choice)
 elif inverse_choice == 6:
-    decision = 'arccosec'
-    calculated_output1, calculated_output2 = arcsin(1/number_choice)
+    decision = 'arc cosec'
+    calculated_output = arc_sin(1/number_choice)
 else:
     decision = 'Unidentified Function!'
+    calculated_output = "Unidentified Value"
 
-print(f'{decision} of {number_choice} is {calculated_output1} and/or {calculated_output2}')
+print(f"{decision} of {number_choice} is '{calculated_output}'")
